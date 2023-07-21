4416ef183f01c7e78b54e157c9f7d1b55eabf9cf
praise adeyemi <praise@ltforce.com>
2023-07-21 14:38:00 -0700

    ok

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print square- function that prints squares
+ * @size: size of square
+ * We can ony use _puchar to print and we are to use # to print square
+ */
+
+void print_square(int size)
+{
+	int x, y;
+
+	y = 0
+
+		if (size < 1)
+			_ptchar('\n');
+		while (y < size)
+		{
+			x = 0;
+			while (x < size)
+			{
+				_putchar ('#');
+				x++;
+			}
+			_putchar ('\n');
+			y++;
+		}
+
