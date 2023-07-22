513b17b34cb02cef495477027aad431357187cf5
praise adeyemi <praise@ltforce.com>
2023-07-21 17:01:38 -0700

    ok

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -4,16 +4,27 @@
  * print_triangle - prints a triangle, followed by a new line
  * @size: size of the triangle
  */
+
 void print_triangle(int size)
 {
-	if (size <= 0)
-		_putchar('\n');
-	else
-		int i, j;
-	for (i = 1; i <= size; i++)
-	for (j = i; j < size; j++)
-		_putchar(' ');
-	for (j = 1; j <= i; j++)
-		_putchar('#');
-		_putchar('\n');
-}
+	int hash, index;
+
+	if (size > 0)
+	{
+		for (hash <= size; hash++)
+		{
+			for (index = size - hash; index > 0; index--)
+			{
+				_putchar(' ');
+			}
+			for (index = 0; index < hash; index++)
+			{
+				_putchar('#');
+			}
+			if (hash == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+
