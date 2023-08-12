dd65fc107707acb35984190dcc78c3494efbf02e
praise adeyemi <praise@ltforce.com>
2023-08-12 06:04:43 -0700

    ok

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,19 @@
 #include "main.h"
 
 /**
-*  _puts - this program prints a string to stout
-* @str: The string to be printed
-* _putchar: prints a new line
-**/
+ * _puts - print string follow by new line
+ * @str: char to check
+ * Owned by Steveric1
+ * Return: 0 on success
+ */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int n = 0;
+
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n++;
+	}
 	_putchar('\n');
 }
