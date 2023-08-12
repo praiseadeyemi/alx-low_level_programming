f3a7779b4eaf878e7f2a5034e0634c9da1cae90f
praise adeyemi <praise@ltforce.com>
2023-08-12 05:21:05 -0700

    ok

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,13 +1,13 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
+
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-int main(void)
+*  _puts - this program prints a string to stout
+* @str: The string to be printed
+* _putchar: prints a new line
+**/
+void _puts(char *str)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	while (*str)
+		_putchar(*str++);
+	_putchar('\n');
 }
